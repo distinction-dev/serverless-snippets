c1a82efbc3a9c4d8fd3673e4416ec6b376d093ad
KillDozerX2 <himanshupant127@gmail.com>
2022-11-22 15:47:18 +0530

    feat: :rocket: initialized the repository and basic snippets

---
+++ README.md
@@ -0,0 +1,10 @@
+# Serverless Framework snippets for VS Code
+
+This extension contains code snippets for YAML syntax for Vs Code editor.
+
+## Installation
+
+In order to install an extension you need to launch the Command Pallete (Ctrl + Shift + P or Cmd + Shift + P) and type Extensions.
+There you have either the option to show the already installed snippets or install new ones. Search for *Serverless Snippets* and install it.
+
+## Snippets
---
+++ build.js
@@ -0,0 +1,46 @@
+const fs = require("fs")
+const path = require("path")
+
+
+/**
+ * 
+ * @param {Array<fs.PathLike>} snippets 
+ * @param {string} outFile
+ * @param {string} [scope]
+ */
+function compileSnippets(snippets, outFile, scope) {
+    let result = {}
+    snippets.map(s => JSON.parse(fs.readFileSync(s).toString())).forEach(s => {
+        if (scope) {
+            Object.keys(s).forEach(k => {
+                s[k].scope = scope
+            })
+        }
+        result = {
+            ...s
+        }
+    })
+    fs.writeFileSync(path.join(__dirname, "snippets", outFile), JSON.stringify(result, null, 4))
+    fs.writeFileSync(
+        path.join(__dirname, ".vscode", `${outFile}.code-snippets`),
+        JSON.stringify(result, null, 4)
+    )
+}
+
+/**
+ * This function scans the yaml directory in snippets directory and combines them into a single json file
+ */
+function buildYaml() {
+    const files = fs.readdirSync(path.join(__dirname, "snippets", "yaml"))
+        .map(f => path.join(__dirname, "snippets", "yaml", f))
+    compileSnippets(files, "yaml.json", "yaml")
+    // copy to .vscode as well
+}
+
+function run() {
+    buildYaml()
+}
+
+if (require.main === module) {
+    run()
+}
